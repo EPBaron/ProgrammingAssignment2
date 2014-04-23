9b91ffb7ab7242784a9c1cd96cdbc0d317591c51
EPBaron <ephraim@epbaron.com>
2014-04-22 21:46:06 -0700

    Completed code for Programming Assignment 2.  Tested functions. All functioned correctly.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Function to create a special matrix vector which 
+## returns a list of 4 functions:
+## 1) set the value of the matrix
+## 2) get the value of the matrix
+## 3) set the value of the inverse
+## 4) get the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+    # initialize inverse to NULL
+    inverse <- NULL
 
+    # 1) set the value of the matrix
+    set <- function(y) {
+        x <<- y
+        # NULL inverse variable since matrix is new
+        inverse <<- NULL
+    }
+    
+    # 2) get the value of the matrix
+    get <- function() x
+    
+    # 3) set the value of the inverse
+    setInverse <- function(inverse) inverse <<- inverse
+    
+    # 4) get the value of the inverse
+    getInverse <- function() inverse
+
+    # return a vector of functions 1-4
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## Function to calculate the inverse of the special matrix
+## created with the makeCacheMatrix function above.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    # first check to see if the inverse has already been calculated
+    inverse <- x$getInverse()
+    if(!is.null(inverse)) {
+        message("getting cached data")
+        return(inverse)
+    }
+    # since inverse not cached, read matrix into local variable
+    data <- x$get()
+    # compute inverse of matrix
+    inverse <- solve(data, ...)
+    # cache the inverse for future lookups
+    x$setInverse(inverse)
+    # return the value of matrix inverse
+    inverse
 }
+
